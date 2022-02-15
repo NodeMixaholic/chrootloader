d03ed1eb7815cc5195ebfbec980b5076a2dfde22
Samuel Lord <sam@sllord.info>
2022-02-14 21:19:05 -0600

    Update chrootloader.sh

--- chrootloader.sh
+++ chrootloader.sh
@@ -3,7 +3,7 @@ mkdir ~/root$id
 sudo mount /dev/mmcblk0p2 ~/root$id
 sudo mkdir ~/root$id/run/systemd
 sudo echo "nameserver 1.1.1.1" > /etc/resolv.conf
-sudo cp /run/systemd/resolve ~/root$id/run/systemd
+sudo cp -r /run/systemd/resolve ~/root$id/run/systemd
 sudo chroot ~/root$id
 sudo umount /dev/mmcblk0p2
 rm -r ~/root$id
