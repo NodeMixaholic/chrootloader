272243422c48ac5903bf990b5fa20e3c83e669b2
Samuel Lord <sam@sllord.info>
2022-02-14 21:25:17 -0600

    Update chrootloader.sh

--- chrootloader.sh
+++ chrootloader.sh
@@ -1,9 +1,15 @@
+# Set an ID, make a mount point, and chroot into the drive.
 id=$RANDOM
 mkdir ~/root$id
 sudo mount /dev/mmcblk0p2 ~/root$id
+# Setup networking
 sudo mkdir ~/root$id/run/systemd
 sudo echo "nameserver 1.1.1.1" > /etc/resolv.conf
 sudo cp -r /run/systemd/resolve ~/root$id/run/systemd
+# Clear the screen and chroot
+clear
 sudo chroot ~/root$id
+# Unmount after exit command issued or error occurs.
 sudo umount /dev/mmcblk0p2
+# Remove the mount point because we don't need it anymore.
 rm -r ~/root$id
